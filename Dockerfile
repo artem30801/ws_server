c04c46f46308366b0d932e85d225eba16d87f84e
Artem Batalov <a@batalov.me>
2022-04-15 23:05:22 +0300

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 EXPOSE 80
-CMD ["uvicorn", "app.server", "--host", "0.0.0.0", "--port", "80"]
+COPY . /code
+CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "80"]
--- sender.py
+++ sender.py
@@ -1,11 +1,28 @@
+import json
+
 import requests
 
+# In initialization section:
+host = 'http://135.181.81.30:8090'
+s = requests.Session()
+s.auth = ('Participant-1', 'pass')
 
-session = requests.Session()
-session.auth = ('Participant-1', 'pass')
+# ... (some code)
 
-data = {"item": "Laptop", "coordinates": [1.1, 2.2, 3.3], "section": 3, "correct": True}
-response = session.post("http://localhost:8000/storage/report/detected", json=data, verify=False)
+# When you need to send the info about an object
+data = {
+    "item": "Laptop",
+    "coordinates": [1.1, 2.2, 3.3],
+    "section": 1,
+    "correct": True
+}
+r = s.post(host + "/storage/report/detected", json=data, verify=False)
+if r.status_code != 200:  # if error
+    print(r)
 
-session.post("http://localhost:8000/storage/report/total/1", params={"count": 3}, verify=False)
-session.post("http://localhost:8000/storage/report/total/2", params={"count": 5}, verify=False)
+# Update total section count
+r = s.post(host + "/storage/report/total/1", params={"count": 1}, verify=False)
+#                                        ^                    ^
+#                                  section_number       total_count_in_section
+if r.status_code != 200:  # if error
+    print(r)
